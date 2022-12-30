4dab2140f79b7fcdabc9759d9f4876519613ab9c
grishanova-oa <grishanova.oa@gmail.com>
2022-12-30 09:42:13 +0100

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './FooterStyles.css';
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-link__git">
+        <a
+          href="https://github.com/grishanova-oa"
+          target="_blank"
+          className="link-text"
+          rel="noreferrer"
+        >
+          GIt Author Olga
+        </a>
+        <a
+          href="https://github.com/grishanova-oa"
+          target="_blank"
+          className="link-text"
+          rel="noreferrer"
+        >
+          GIt Author Dzmitry
+        </a>
+      </div>
+      <p>2023</p>
+      <a className="footer__logo" href="https://rs.school/js/">
+        RSS
+      </a>
+    </div>
+  );
+};
