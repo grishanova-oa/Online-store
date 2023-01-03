988acc53dd9e8d6ef8fcf0e1bb9d26a1bc610348
KhomDV <khomdv@mail.ru>
2023-01-03 17:55:58 +0300

    feat: start page main

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import { Main } from './components/Pages/Main/Main';
 import './styles.css';
 
 export const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
