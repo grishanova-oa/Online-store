3f5e7bc48df87d275202ecfb8aba1445840853b6
grishanova-oa <grishanova.oa@gmail.com>
2023-01-08 23:13:10 +0100

    feat: addgrand  image to page

---
+++ src/components/InfoGrangPhoto/InfoGrangPhoto.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './InfoGrangPhotoStyles.css';
+
+export const InfoGrangPhoto = () => {
+  return (
+    <div className="grand-photo">
+      <img
+        src="https://i.dummyjson.com/data/products/1/4.jpg"
+        alt="imagegrand"
+        className="grand-photo__img"
+      />
+    </div>
+  );
+};
